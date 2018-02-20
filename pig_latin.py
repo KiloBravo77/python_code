792b923dfa63f832c5fba2e9ee5c903de9209d92
Kyle Brown <kylebrown@Kyles-MBP.fios-router.home>
2018-02-19 22:18:21 -0500

    Updated logic for second letter = y

--- pig_latin.py
+++ pig_latin.py
@@ -7,10 +7,13 @@ if len(original) > 0 and original.isalpha():
 
 	if first_letter == 'a' or first_letter == 'e' or first_letter == 'i' or first_letter == 'o' or first_letter == 'u':
 		pig_word = word + 'way'
-	elif second_letter != 'a' and second_letter != 'e' and second_letter != 'i' and second_letter != 'o' and second_letter != 'u':
+	elif second_letter != 'a' and second_letter != 'e' and second_letter != 'i' and second_letter != 'o' and second_letter != 'u' and second_letter != 'y':
 		pig_word = word[2:] + first_letter + second_letter + 'ay'
 	else:
-		pig_word = word[1:] + first_letter + 'ay'
+		if second_letter == 'y':
+			pig_word = 'i' + word[2:] + first_letter + 'ay'
+		else:
+			pig_word = word[1:] + first_letter + 'ay'
 
 	print(pig_word)
 else:
