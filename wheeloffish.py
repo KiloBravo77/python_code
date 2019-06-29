96d1e7ce16f2629395db69712895cf81cf08e023
Kyle Brown <kbrown@system76-pc.localdomain>
2019-06-28 22:51:32 -0400

    Update for python 3-changed raw_input to input. Fixed duplicate prompt

--- wheeloffish.py
+++ wheeloffish.py
@@ -1,9 +1,9 @@
 def wheeloffish():
 	print("Spin the wheel of fish!")
 	print("ooooo red snappah. vetty tasty!")
-	print("Do you want the fish or the box?")
+	#print("Do you want the fish or the box?")
 
-	answer = raw_input("Do you want the fish or the box?").lower()
+	answer = input("Do you want the fish or the box?").lower()
 
 	if answer == "box":
 		print("YOU TOOK THE BOX! WHAT'S IN THE BOX???")
