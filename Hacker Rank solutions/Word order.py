5489f5c329b98a5310d2ba886971d4196018b6ad
Kyle Brown <kbrown@system76-pc.localdomain>
2019-06-29 21:19:52 -0400

    Removing for loop used to print values

--- Hacker Rank solutions/Word order.py
+++ Hacker Rank solutions/Word order.py
@@ -6,11 +6,5 @@ for _ in range(int(input())):
 	word = input()
 	word_list.append(word)
 
-word_dict = Counter(word_list).values()
-word_counts = ""
-
-for value in word_dict:
-	word_counts = word_counts + " " + str(value)
-
 print(len(Counter(word_list).keys()))
-print(word_counts[1:])
+print(*Counter(word_list).values())
