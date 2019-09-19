d28a60db6f434936cdaa467d00354e9124074ab9
Kyle Brown <kbrown@system76-pc.localdomain>
2019-09-19 19:38:44 -0400

    latest changes

--- Hacker Rank solutions/text_wrap.py
+++ Hacker Rank solutions/text_wrap.py
@@ -3,7 +3,6 @@ import textwrap
 def wrap(string, max_width):
 	wrapped_word = ""
 	for word in textwrap.wrap(string, max_width):
-		#print(word)
 		wrapped_word += word + "\n"
 	return wrapped_word
 
